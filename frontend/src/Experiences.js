afabbe38a1c6469e73c51d88f175c777ec3a9634
JoachimBjerkland <111729453+JoachimBjerkland@users.noreply.github.com>
2024-10-14 15:16:52 +0200

    update

---
+++ frontend/src/Experiences.js
@@ -0,0 +1,13 @@
+// src/components/Experiences.js
+import React from 'react';
+
+const Experiences = () => {
+  return (
+    <div>
+      <h1>Experiences Component</h1>
+      <p>Dette er en liste over erfaringer.</p>
+    </div>
+  );
+};
+
+export default Experiences;
